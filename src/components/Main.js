037788e7c80e2e0b652ce61994ddb13b22fff454
larry <918035829@qq.com>
2017-01-04 17:18:51 +0800

    功能完成

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,75 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
+import React, { Component, PropTypes } from 'react'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
 
-import React from 'react';
+// React component
+class Counter extends Component {
+    render() {
+        const { value, onIncreaseClick, onDecreaseClick } = this.props
+        return (
+            <div>
+                <button onClick={onDecreaseClick}>Decrease</button>
+                <span>{value}</span>
+                <button onClick={onIncreaseClick}>Increase</button>
+            </div>
+        )
+    }
+}
 
-let yeomanImage = require('../images/yeoman.png');
+Counter.propTypes = {
+    value: PropTypes.number.isRequired,
+    onIncreaseClick: PropTypes.func.isRequired
+}
 
-class AppComponent extends React.Component {
+// Action
+const increaseAction = { type: 'increase' }
+const decreaseAction = { type: 'decrease' }
+
+// Reducer
+function counter(state = { count: 0 }, action) {
+    const count = state.count
+    switch (action.type) {
+        case 'increase':
+            return { count: count + 1 }
+        case 'decrease':
+            return { count: count - 1 }
+        default:
+            return state
+    }
+}
+
+// Store
+const store = createStore(counter)
+
+// Map Redux state to component props
+function mapStateToProps(state) {
+    return {
+        value: state.count
+    }
+}
+
+// Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+    return {
+        onIncreaseClick: () => dispatch(increaseAction),
+        onDecreaseClick: () => dispatch(decreaseAction)
+    }
+}
+
+// Connected Component
+const App = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter)
+
+class AppComponent extends Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+        <Provider store={store}>
+            <App />
+        </Provider>
     );
   }
 }
-
-AppComponent.defaultProps = {
-};
 
 export default AppComponent;
